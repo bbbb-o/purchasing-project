4456cd99f3c137bfe76cc9222078ddb10b6bdab4
bbbb-o <Snowzhao0930@163.com>
2026-06-07 16:59:52 +0800

    修正下载路径 + 移动端fetch/blob下载方案

--- functions/api/download/[fid].js
+++ functions/api/download/[fid].js
@@ -11,6 +11,7 @@ export async function onRequest(context) {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Expose-Headers': 'Content-Disposition, Content-Type, Content-Length',
   };
 
   if (request.method === 'OPTIONS') {
